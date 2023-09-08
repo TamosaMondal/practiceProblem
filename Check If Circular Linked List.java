39b2c1610c50dbe34d044e9393c73154e2043311
Tamosa <77878238+TamosaMondal@users.noreply.github.com>
2023-09-08 12:19:39 +0530

    Create Check If Circular Linked List.java

---
+++ Check If Circular Linked List.java
@@ -0,0 +1,21 @@
+/*Given head, the head of a singly linked list, find if the linked list is circular or not. A linked list is called circular if it not NULL terminated and all nodes are connected in the form of a cycle. An empty linked list is considered as circular.
+
+Note: The linked list does not contains any inner loop.
+*/
+class GfG
+{
+    boolean isCircular(Node head)
+    {
+	// Your code here
+	Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
